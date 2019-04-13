18da80e596f78c2e3a4e7f7c362570370f8d9b2f
Vedran Bartonicek <vedran.bartonicek@tieto.com>
2019-04-13 16:22:19 +0300

    Improved exporting of environment variables

--- inventory/teco-cluster/terraform-env.sh
+++ inventory/teco-cluster/terraform-env.sh
@@ -7,4 +7,5 @@ echo "Please enter TECO customer name"
 read TECO_CUSTOMER_INPUT
 export TECO_CUSTOMER=$TECO_CUSTOMER_INPUT
 
-
+source  /home/cloud-user/openstack-paladev-rc.sh
+source  /home/cloud-user/openstack_setenv.sh
