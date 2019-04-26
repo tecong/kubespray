57adf3417d6d70df04322bb816758377c11fa80c
Vedran Bartonicek <vedran.bartonicek@tieto.com>
2019-04-26 19:28:14 +0300

    Opening ports for etcd, ssh, icmp

--- contrib/terraform/openstack/modules/compute/main.tf
+++ contrib/terraform/openstack/modules/compute/main.tf
@@ -18,6 +18,16 @@ resource "openstack_networking_secgroup_rule_v2" "k8s_master" {
   security_group_id = "${openstack_networking_secgroup_v2.k8s_master.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "k8s_master-etcd" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = "2379"
+  port_range_max    = "2380"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s_master.id}"
+}
+
 resource "openstack_networking_secgroup_v2" "bastion" {
   name        = "${var.cluster_name}-bastion"
   count       = "${var.number_of_bastions ? 1 : 0}"
@@ -44,6 +54,26 @@ resource "openstack_networking_secgroup_rule_v2" "k8s" {
   direction         = "ingress"
   ethertype         = "IPv4"
   remote_group_id   = "${openstack_networking_secgroup_v2.k8s.id}"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s-icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol = "icmp"
+  port_range_min = "0"
+  port_range_max = "0"
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s-ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol = "tcp"
+  port_range_min = "22"
+  port_range_max = "22"
+  remote_ip_prefix = "0.0.0.0/0"
   security_group_id = "${openstack_networking_secgroup_v2.k8s.id}"
 }
 
